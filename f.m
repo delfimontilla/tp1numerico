10e9d0389155ce99047f195447543ca76ab6fcb2
alejoacevedo <ale.acevedo@live.com.ar>
2018-09-18 11:54:24 -0300

    Termino la funcion discretizar

---
+++ f.m
@@ -0,0 +1,2 @@
+function resultado  = f(x, y)  if y==0 || y==1    resultado = 0;  elseif x==0 || x==1    resultado = y * (1 - y);  else    resultado = NaN;  endif
+endfunction
--- obtener_L.m
+++ obtener_L.m
@@ -1,1 +1,1 @@
-function resultado = obtener_U(A)  nFil = size(A)(1);  nCol = size(A)(2);  resultado = zeros(nFil, nCol);  for i = 1:nFil    for j = 1:nCol      if j < i        resultado(i,j) = A(i,j);      endif    endfor  endforendfunction
+function resultado = obtener_L(A)  nFil = size(A)(1);  nCol = size(A)(2);  resultado = zeros(nFil, nCol);  for i = 1:nFil    for j = 1:nCol      if j < i        resultado(i,j) = A(i,j);      endif    endfor  endforendfunction
