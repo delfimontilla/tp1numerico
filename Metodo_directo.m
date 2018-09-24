91c034c2fbbc5c688f8d1a6528abe08e4d37cf31
delfimontilla <37596500+delfimontilla@users.noreply.github.com>
2018-09-24 17:12:30 -0300

    LU

--- Metodo_directo.m
+++ Metodo_directo.m
@@ -1,7 +1,20 @@
 function x = metodo_directo(A,b)
 
- L = obtener_L(A);
- U = obtener_U(A);
+  m = length(A);
+  
+  U = A;
+  L = eye(m, m);
+  
+  for k = 1 : m - 1
+  
+    for j = (k + 1) : m
+    
+      L(j, k) = U(j, k) / U(k,k);
+      U(j, k : m) = U(j, k : m) - L(j, k) * U(k, k : m);
+    
+    endfor
+    
+endfor
  
  n = length(L);
  y = zeros(n, 1);
