e14af4b08341a859d0840d0286c56dfced6bce27
Alejo <ale.acevedo@live.com.ar>
2018-09-24 12:06:57 -0300

    modifico jacobi

--- metodo_jacobi.m
+++ metodo_jacobi.m
@@ -1,11 +1,15 @@
 function resultado = metodo_jacobi(A, b)
+  error = 0.00001;
+  cantMaxima = 500;
+  i = 0;
   nfilas = size(A)(1);
   ncolumnas = size(A)(2);
   resultado = repmat(0, 1 , ncolumnas)';
   D = inv(obtener_D(A));
   U = obtener_U(A);
   L = obtener_L(A);
-  while(all((abs((A*resultado) - b) > 0.00001)))
-    resultado = D*b - D*(L+U)*resultado
+  while(any(abs((A*resultado)-b)>error) - b && i < cantMaxima)
+    resultado = D*b - D*(L+U)*resultado;
+    i = i + 1
   endwhile
 endfunction
