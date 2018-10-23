42bd41089d8482fd16e73bfedb698f8c6a6c6e49
delfimontilla <delfinamontilla@gmail.com>
2018-10-23 17:23:28 -0300

    Graf

--- graficarErrorGauss.m
+++ graficarErrorGauss.m
@@ -1,9 +1,15 @@
+clear all, close all;
+pkg load optim;
+
+graphics_toolkit gnuplot;
+
 function graficarErrorGauss()
   errorGauss = csvread("errorGauss.csv");
   figure('papersize', [40 40])
   plot(errorGauss, 'linewidth', 1);
   xlabel("Cantidad de iteraciones", 'fontsize', 50);
   ylabel("Error", 'fontsize', 50);
-  title("Error VS Cantidad De Iteraciones",  'fontsize', 50);
+  title("Error VS Cantidad De Iteraciones",  'fontsize', 20);
   axes("xtick", 0:10:100);
+  print('GrafErrorGauss.png','-dpng','-r300')
 end
